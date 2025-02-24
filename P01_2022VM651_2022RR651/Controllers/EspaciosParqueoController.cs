4f71afae5a113dd108454f03773005488e71a9ca
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-02-24 17:39:02 -0600

    Cambie nombre

---
+++ P01_2022VM651_2022RR651/Controllers/EspaciosParqueoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P01_2022VM651_2022RR651.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022VM651_2022RR651.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EspaciosParqueoController : ControllerBase
+    {
+        private readonly ParkingDBContext _ParkinDbcontext;
+
+        public EspaciosParqueoController(ParkingDBContext ParkinDbcontext)
+        {
+            _ParkinDbcontext = ParkinDbcontext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<EspaciosParqueo> listadoEspaciosParquo = (from ep in _ParkinDbcontext.Espacios select ep).ToList();
+
+            if(listadoEspaciosParquo.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEspaciosParquo);
+        }
+
+
+    }
+}
