940d3a1dce869749525ef85cfa45b89ab300b5b2
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-02-24 17:21:13 -0600

    Modelo Usuarios hecho

--- P01_2022VM651_2022RR651/Models/Usuarios.cs
+++ P01_2022VM651_2022RR651/Models/Usuarios.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace P01_2022VM651_2022RR651.Models
 {
     public class Usuarios
     {
-
+        [Key]
+        public int UsuarioID { get; set; }
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Rol { get; set; }
     }
 }
