2786b68b16d00fb692f50fe8bc1709efe34464a6
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-02-24 17:50:32 -0600

    Sucursales modelos hecho

---
+++ P01_2022VM651_2022RR651/Models/Sucursales.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022VM651_2022RR651.Models
+{
+    public class Sucursales
+    {
+        [Key]
+        public int SucursalID { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public int AdministradorID { get; set; }
+        public int? NumeroEspacios { get; set; }
+    }
+}
