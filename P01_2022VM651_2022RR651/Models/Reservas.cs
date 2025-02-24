ae05259674c65ad6a419725d0e94484e08c4767b
Rodolfo Rivas Rodriguez <128103152+FOFOCode@users.noreply.github.com>
2025-02-24 17:24:12 -0600

    Reservas modelo hecho

---
+++ P01_2022VM651_2022RR651/Models/Reservas.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022VM651_2022RR651.Models
+{
+    public class Reservas
+    {
+        [Key]
+        public int ReservaID { get; set; }
+        public int UsuarioID { get; set; }
+        public int EspacioID { get; set; }
+        public DateTime? FechaReserva { get; set; }
+        public string? Estado { get; set; }
+    }
+}
